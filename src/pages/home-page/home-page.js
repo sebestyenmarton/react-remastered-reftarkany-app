c1060cb3ec04b08ade16320de9c070377b4fb507
Sebestyen Marton <sdgmarton@gmail.com>
2022-05-06 16:23:31 +0300

    daily bible section implementations for home-page

--- src/pages/home-page/home-page.js
+++ src/pages/home-page/home-page.js
@@ -17,7 +17,22 @@ const HomePage = () => {
   }, []);
 
   function DailyBibleSection() {
-    return <div className="daily-bible-section"></div>;
+    return (
+      <div className="daily-bible-section">
+        <div class="daily-date">Jún.10</div>
+        <div class="daily-text">
+          „A győzedelmesnek enni adok az elrejtett mannából..”Jel 2,17
+        </div>
+        <div class="full-daily-container">
+          <div class="daily-description">
+            A teljes napi áhitat a fenti igével a következő gombra kattintva
+            megtekinthető
+          </div>
+          <div class="separation-line" />
+          <div class="daily-button">Napi áhitat</div>
+        </div>
+      </div>
+    );
   }
 
   function HomeCenterSection() {
