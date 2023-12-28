db9f2d813fc3dc6574606bc6d05766da804fe76f
Sebestyen Marton <sdgmarton@gmail.com>
2023-12-28 15:38:45 +0200

    the commit left out of the last PR with resolved conflicts

--- src/components/pages/elerhetoseg-page/contact-page.tsx
+++ src/components/pages/elerhetoseg-page/contact-page.tsx
@@ -1,33 +1,17 @@
-import React , {useState} from "react";
-
-import Navbar from "../../navbar/navbar";
+import React from "react";
 
 import { routingConfiguration } from "../../../service/WebUrlMapper";
+
 import "./contact-page.scss";
-<<<<<<< HEAD
-import { SassColor } from "sass";
+import Navbar from "../../navbar/navbar";
+
 import { FaUserCircle } from "react-icons/fa";
-import { useNavigate } from "react-router-dom";
-
-
-=======
->>>>>>> main
 
 const ContactPage = () => {
- let Map = document.getElementById("Map")
-
- function openMap(){
-Map?.classList.add("openMap")
- }
-
- let navigate = useNavigate(); 
-  const routeChange = (path: string) =>{ 
-
-    navigate(path);
-  }
+  const mapHref =
+    "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2739.5555932656225!2d22.360271975927123!3d46.63554115452835!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4748b01950d0c933%3A0x67ee16b57ad0d8a6!2sBiserica%20Reformat%C4%83!5e0!3m2!1sen!2sro!4v1700331541428!5m2!1sen!2sro";
 
   return (
-    
     <div className="contact-page" id="Contact">
       <Navbar
         selectedValue="elerhetoseg"
@@ -37,42 +21,49 @@ Map?.classList.add("openMap")
         <div className="background1">
           <div className="contact-map">
             <div className="contact-button">
-            <div className="contact">
-              <div className="main">
-                <div className="profil">
-                <FaUserCircle className="user"/></div>
-                <div className="title">ELÉRHETŐSÉGEINK </div>
+              <div className="contact">
+                <div className="main">
+                  <div className="profil">
+                    <FaUserCircle className="user" />
+                  </div>
+                  <div className="title">ELÉRHETŐSÉGEINK </div>
+                </div>
+                <div className="title-contact">Lelkipásztor elérhetőségei</div>
+                <div className="text1">
+                  Sebestyén László Ede <br />
+                  Tel. (004)-0773-325-322 <br /> E-mail:
+                  <span className="e-mail"> sledelp@gmail.com</span>
+                </div>
+                <div className="title-post">Hivatalos postacím</div>
+                <div className="text2">
+                  România, Jud. Bihor,
+                  <br /> Tărcaia, nr. 56,
+                  <br /> COD.: 417575,
+                  <br /> Parohia Reformată
+                </div>
               </div>
-              <div className="title-contact">Lelkipásztor elérhetőségei</div>
-              <div className="text1">
-                Sebestyén László Ede <br />
-                Tel. (004)-0773-325-322 <br /> E-mail:
-                <span className="e-mail"> sledelp@gmail.com</span>
-              </div>
-              <div className="title-post">Hivatalos postacím</div>
-              <div className="text2">
-                România, Jud. Bihor,
-                <br /> Tărcaia, nr. 56,
-                <br /> COD.: 417575,
-                <br /> Parohia Reformată
-              </div>
-             
-            </div>
-            <button  className="button" >
-               Hely a térképen 
-            </button>
+              <button
+                className="button"
+                onClick={() => {
+                  window.open(mapHref);
+                }}
+              >
+                Hely a térképen
+              </button>
             </div>
             <div className="map" id="Map">
-            
-            <iframe  className="map-iframe" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2739.5555932656225!2d22.360271975927123!3d46.63554115452835!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4748b01950d0c933%3A0x67ee16b57ad0d8a6!2sBiserica%20Reformat%C4%83!5e0!3m2!1sen!2sro!4v1700331541428!5m2!1sen!2sro" width="600" height="450"  loading="lazy"></iframe>
-            
+              <iframe
+                title="contact-location-on-map"
+                className="map-iframe"
+                src={mapHref}
+                width="600"
+                height="450"
+                loading="lazy"
+              ></iframe>
             </div>
-            
-
           </div>
-          
         </div>
-        
+
         <div className="background2"></div>
       </div>
     </div>
